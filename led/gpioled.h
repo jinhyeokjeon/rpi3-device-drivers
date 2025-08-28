1900a9bd9518e9581e44bba20fbd5dab7bd08d08
jinhyeokjeon <jinhyeokjeoon@gmail.com>
2025-08-28 16:45:31 +0900

    chore: add #ifndef

--- led/gpioled.h
+++ led/gpioled.h
@@ -1,3 +1,6 @@
+#ifndef __GPIOLED_H_
+#define __GPIOLED_H_
+
 #include <linux/ioctl.h>
 
 #define GPIO_IOC_MAGIC 'l'
@@ -5,3 +8,5 @@
 #define LED_MODE_NORMAL  _IO(GPIO_IOC_MAGIC, 0)
 #define LED_MODE_BLINK   _IO(GPIO_IOC_MAGIC, 1)
 #define LED_SET_PERIOD   _IOW(GPIO_IOC_MAGIC, 2, int)
+
+#endif
